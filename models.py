2a504a09cf14029055f760766f2aee1f82bd4fd8
Maurits <mcb.westbroek@gmail.com>
2022-11-27 11:52:16 +0000

    Update CA __str__ to have two decimal places

--- models.py
+++ models.py
@@ -128,7 +128,7 @@ class CloseApproach:
         # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return f"{self._designation} passed Earth at {self.time_str}, at a distance of {self.distance} au and velocity of {self.velocity} km/s"
+        return f"{self._designation} passed Earth at {self.time_str}, at a distance of {self.distance:.2f} au and velocity of {self.velocity:.2f} km/s"
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""

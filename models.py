e4b69733f6706f47f5d6b6af6209d157a2ef8191
Maurits <mcb.westbroek@gmail.com>
2022-12-02 16:44:01 +0000

    Implement serialize method

--- models.py
+++ models.py
@@ -74,6 +74,15 @@ class NearEarthObject:
         return f"NearEarthObject(designation={self.designation!r}, name={self.name!r}, " \
                f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
 
+    def serialize(self):
+        """Return serialised representation"""
+        return {
+            "designation": self.designation,
+            "name": self.name,
+            "diameter_km": self.diameter,
+            "potentially_hazardous": self.hazardous
+            }
+
 
 class CloseApproach:
     """A close approach to Earth by an NEO.
@@ -134,3 +143,11 @@ class CloseApproach:
         """Return `repr(self)`, a computer-readable string representation of this object."""
         return f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, " \
                f"velocity={self.velocity:.2f}, neo={self.neo!r})"
+    
+    def serialize(self):
+        """Return serialised representation"""
+        return {
+            "datetime_utc": datetime_to_str(self.time),
+            "distance_au": self.distance,
+            "velocity_km_s": self.velocity
+            }
